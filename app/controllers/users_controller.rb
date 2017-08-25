e86272d9ef851abc1aca470aa088af41c8672c25
colkar99 <colkar99@gmail.com>
2017-08-25 22:38:45 +0530

    complete authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_user, except: [:index,:show]
+	
 	before_action :require_same_user, only:[:edit, :update]
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success] = "Welcome to app-blog #{@user.username}"
-			redirect_to articles_path
+			redirect_to user_path(@user)
 		else
 		render 'new'	
 		end
@@ -42,7 +43,7 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 	def require_same_user
-		if current_user != @user
+		if !logged_in? && current_user != @user
 			flash[:danger] = "You can only edit your own profile"
 			redirect_to root_path
 		end
